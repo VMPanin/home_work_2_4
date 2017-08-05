2579816a956207a8fe48199c4ad5f498aed9083f
Panin Vasily <vmpanin92@gmail.com>
2017-08-05 22:29:44 +0300

    Исправил по PEP8, убрал лишнее сравнение с True, убрал вызов функции в цикле

--- home_work_2_4.py
+++ home_work_2_4.py
@@ -1,26 +1,30 @@
 import os
 
+
 def get_current_dir():
-    current_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Migrations\\')
+    current_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Migrations')
     return current_dir
+
 
 def get_sql_files():
     files = os.listdir(get_current_dir())
-    sql_files =[]
+    sql_files = []
     for file in files:
-        if file.endswith('.sql') == True:
+        if file.endswith('.sql'):
             sql_files.append(file)
     return sql_files
 
+
 def find_file():
     files = get_sql_files()
+    current_dir = get_current_dir()
     while True:
         find_str = input('Введите строку из файла:')
         for file in files:
-            with open(os.path.join(get_current_dir(), file)) as f:
+            with open(os.path.join(current_dir, file)) as f:
                 read_file = f.read()
-                if (find_str in read_file) == False:
+                if find_str not in read_file:
                     files.remove(file)
-        print('\n'.join(files),'\nВсего:',len(files))
+        print('\n'.join(files), '\nВсего:', len(files))
 
 find_file()
